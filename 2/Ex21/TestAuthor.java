f86ed0c90f9f890e5c36b913a8f3d56f54216a87
IvanovP3 <116543608+IvanovP3@users.noreply.github.com>
2022-10-24 10:05:53 +0300

    Delete 2 directory

--- 2/Ex21/TestAuthor.java
+++
@@ -1,8 +0,0 @@
-public class TestAuthor {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-		Author Sam = new Author("Sam", "sam@yahoo.com", 'm');
-		System.out.println(Sam.toString());
-	}
-
-}
